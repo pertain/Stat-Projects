1aaa281f8cdbce1369974649994a64b720f9f075
william <4yellowpeppers@gmail.com>
2015-03-29 12:14:30 -0400

    formatting cleanup

--- project02/proj2.r
+++ project02/proj2.r
@@ -30,9 +30,9 @@ q4kwh <- qtr4$kwh
 #q4kwh
 
 sageColors <- c("darkseagreen4","darkseagreen","darkseagreen3",
-				"darkseagreen2","darkseagreen1","honeydew",
-				"white","honeydew","darkseagreen1",
-				"darkseagreen2","darkseagreen3","darkseagreen")
+                "darkseagreen2","darkseagreen1","honeydew",
+                "white","honeydew","darkseagreen1",
+                "darkseagreen2","darkseagreen3","darkseagreen")
 
 
 # ===========================================================
@@ -40,9 +40,9 @@ sageColors <- c("darkseagreen4","darkseagreen","darkseagreen3",
 # ===========================================================
 
 stem.leaf.backback(x = q1kwh,
-				   y = q2kwh,
-				   m = 1,
-				   unit = 10)
+                   y = q2kwh,
+                   m = 1,
+                   unit = 10)
 
 
 # ===========================================================
@@ -51,11 +51,11 @@ stem.leaf.backback(x = q1kwh,
 
 png("hist_qtr1_kwh.png")
 hist(qtr1$kwh,
-	 #col = cm.colors(7),
-	 #col = terrain.colors(7),
-	 col = sageColors,
-	 main = "Histogram of First Quarter\nElectricity Usage",
-	 xlab = "Electricity Usage [kWh]")
+     #col = cm.colors(7),
+     #col = terrain.colors(7),
+     col = sageColors,
+     main = "Histogram of First Quarter\nElectricity Usage",
+     xlab = "Electricity Usage [kWh]")
 dev.off()
 
 
@@ -75,10 +75,10 @@ df$qtrs <- append(rep(c(rep("Q4",3), rep("Q3",3), rep("Q2",3), rep("Q1",3)), 5),
 
 png("boxplots_kwh.png")
 boxplot(df$kwh~df$qtrs,
-		main = "Boxplots of Electricity Usage\nby Quarter",
-		xlab = "Quarters",
-		ylab = "Electricity Usage [kWh]",
-		col = terrain.colors(4))
+        main = "Boxplots of Electricity Usage\nby Quarter",
+        xlab = "Quarters",
+        ylab = "Electricity Usage [kWh]",
+        col = terrain.colors(4))
 dev.off()
 
 
@@ -90,20 +90,20 @@ months <- append(rep(c("Dec","Mar","Jun","Sep"), 5), "Dec")
 
 png("line_dolpkwh.png")
 plot(df$dolpkwh,
-	 type = "l",
-	 axes = FALSE,
-	 main = "Seasonal Trends in Electricity Prices",
-	 font.lab = 2,
-	 xlab = "Months (over a five year period)",
-	 ylab = "Price ($/kWh)",
-	 lwd = 3,
-	 col = "springgreen3")
+     type = "l",
+     axes = FALSE,
+     main = "Seasonal Trends in Electricity Prices",
+     font.lab = 2,
+     xlab = "Months (over a five year period)",
+     ylab = "Price ($/kWh)",
+     lwd = 3,
+     col = "springgreen3")
 grid()
 box()
 axis(side = 1,
-	 at = seq(1,61,3),
-	 lab = months,
-	 las = 2)
+     at = seq(1,61,3),
+     lab = months,
+     las = 2)
 axis(side = 2)
 dev.off()
 
@@ -113,15 +113,15 @@ dev.off()
 # ===========================================================
 
 mons <- append(rep(c("Dec","Nov","Oct","Sep","Aug","Jul",
-					 "Jun","May","Apr","Mar","Feb","Jan"), 5),
-			   c("Dec","Nov","Oct"))
+                     "Jun","May","Apr","Mar","Feb","Jan"), 5),
+               c("Dec","Nov","Oct"))
 
 qplot(kwhpday,
-	  dolpkwh,
-	  colour = mons,
-	  data = df) +
+      dolpkwh,
+      colour = mons,
+      data = df) +
 labs(colour = "Months",
-	 x = "Daily Electricity Usage (kWh/Day)",
-	 y = "Price of Electricity ($/kWh)",
-	 title = "Scatterplot of Electricity Prices vs Usage")
+     x = "Daily Electricity Usage (kWh/Day)",
+     y = "Price of Electricity ($/kWh)",
+     title = "Scatterplot of Electricity Prices vs Usage")
 ggsave(file = "scatter.png")
