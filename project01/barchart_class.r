bda1ffc4cee250cd60978d9748278d490a1cf1e4
william <4yellowpeppers@gmail.com>
2015-03-29 12:10:15 -0400

    formatting cleanup

--- project01/barchart_class.r
+++ project01/barchart_class.r
@@ -1,14 +1,14 @@
 df <- read.csv("sample_data.csv")
 
 numByClass <- c(length(subset(df$yr, df$yr == "Freshman")),
-				length(subset(df$yr, df$yr == "Sophomore")),
-				length(subset(df$yr, df$yr == "Junior")),
-				length(subset(df$yr, df$yr == "Senior")))
+                length(subset(df$yr, df$yr == "Sophomore")),
+                length(subset(df$yr, df$yr == "Junior")),
+                length(subset(df$yr, df$yr == "Senior")))
 
 png("barchart_class.png")
 barplot(numByClass,
-		main="Students by Class\n(barchart)",
-		xlab="Class", ylab="Total Students",
-		col=rainbow(4),
-		names.arg=c("Freshman","Sophomore","Junior","Senior"))
+        main="Students by Class\n(barchart)",
+        xlab="Class", ylab="Total Students",
+        col=rainbow(4),
+        names.arg=c("Freshman","Sophomore","Junior","Senior"))
 dev.off()
--- project01/hist_studyHrs.r
+++ project01/hist_studyHrs.r
@@ -2,17 +2,17 @@ df <- read.csv("sample_data.csv")
 
 png('hist_studyHrs.png')
 hist(df$studyHrs,
-	 col=heat.colors(max(df$inClassHrs)),
-	 main="Hours Spent Studying\n(histogram)",
-	 xlab="Hours spent studying outside of class")
+     col=heat.colors(max(df$inClassHrs)),
+     main="Hours Spent Studying\n(histogram)",
+     xlab="Hours spent studying outside of class")
 abline(v=mean(df$studyHrs),
-	   col="deepskyblue3",
-	   lwd=2)
+       col="deepskyblue3",
+       lwd=2)
 abline(v=median(df$studyHrs),
-	   col="chartreuse3",
-	   lwd=2)
+       col="chartreuse3",
+       lwd=2)
 legend(x="topright",
-	   c("Mean","Median"),
-	   col=c("deepskyblue3","chartreuse3"),
-	   lwd=c(2,2))
+       c("Mean","Median"),
+       col=c("deepskyblue3","chartreuse3"),
+       lwd=c(2,2))
 dev.off()
